b6dcf129c30c48ab527c7e8fe1de3aeb1dfab7fc
mudssky <mudssky@gmail.com>
2024-01-31 22:42:44 +0800

    📃 docs: update docs

--- packages/slidev-style/README.md
+++ packages/slidev-style/README.md
@@ -1,3 +1,16 @@
 # Slidev Style
 
 自定义slidev样式，和theme分开发布，方便不使用theme的时候单独引用。
+
+## 安装
+
+```shell
+pnpm add @muddsky/slidev-style
+```
+
+## 使用
+
+```ts
+import '@mudssky/slidev-style/index'//导入ts
+import '@mudssky/slidev-style/index.css'//导入css
+```
--- packages/slidev-theme-default/styles/index.ts
+++ packages/slidev-theme-default/styles/index.ts
@@ -1,4 +1,6 @@
 // inherit from base layouts, remove it to get full customizations
+import '@mudssky/slidev-style/index'
+import '@mudssky/slidev-style/index.css'
 import '@slidev/client/styles/layouts-base.css'
 import './code.css'
 import './layout.css'
