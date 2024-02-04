be329e77a55c70f48b67fcc588e4e886d16e2ec4
mudssky <mudssky@gmail.com>
2024-02-04 17:32:49 +0800

    ✨ feat: add comments

--- projects/slidevs/learn-css/uno.config.ts
+++ projects/slidevs/learn-css/uno.config.ts
@@ -8,5 +8,7 @@ export default defineConfig({
     ['list-inside', { 'list-style-position': 'inside' }],
     // 配置背景图片,发现unocss和tailwind的预设并不完全兼容，unocss的解析器不支持解析引号的样式
     // 所以  bg-[url(/assets/dust-mountain.avif)] bg-cover 要这样使用
+    // 还发现一个问题，就是预设更新的不及时，现在tailwind 3.4新出的设置子元素样式的unocss这个预设
+    // 还没跟进
   ],
 })
