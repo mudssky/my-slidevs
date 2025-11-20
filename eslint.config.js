27d4c9f1c1d1001a3c0f368e7d4f7b772847ad99
mudssky <mudssky@gmail.com>
2025-11-20 17:43:37 +0800

    chore: 迁移 ESLint 配置至新格式并升级依赖
    删除旧的 .eslintrc.js 文件，新增 eslint.config.js 使用新版 ESLint 扁平化配置格式
    升级 ESLint 及相关插件至最新版本，调整 package.json 中的脚本和依赖

---
+++ eslint.config.js
@@ -0,0 +1,37 @@
+import js from '@eslint/js'
+import eslintConfigPrettier from 'eslint-config-prettier'
+import pluginVue from 'eslint-plugin-vue'
+import vueParser from 'vue-eslint-parser'
+import globals from 'globals'
+import tseslint from 'typescript-eslint'
+
+export default tseslint.config(
+  { ignores: ['**/*.d.ts', '**/coverage/**', '**/dist/**', '**/.vitepress/cache/**', 'node_modules/**', 'projects/slidevs/**'] },
+  {
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.recommended,
+      ...pluginVue.configs['flat/strongly-recommended'],
+    ],
+    files: ['**/*.{ts,vue}'],
+    plugins: {
+      '@typescript-eslint': tseslint.plugin,
+    },
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      parser: vueParser,
+      parserOptions: {
+        parser: tseslint.parser,
+      },
+      globals: {
+        ...globals.browser,
+      },
+    },
+    rules: {
+      'vue/multi-word-component-names': 'off',
+      '@typescript-eslint/no-unused-expressions': 'off',
+    },
+  },
+  eslintConfigPrettier,
+)
