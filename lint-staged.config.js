4cffaad1a7f65e3146d4488188407acdf7cffdb1
mudssky <mudssky@gmail.com>
2025-12-03 15:22:43 +0800

    ci(lint): 添加对Vue文件的格式化和ESLint检查
    在lint-staged配置中增加对Vue文件的支持，使用prettier进行格式化并添加ESLint检查

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -3,9 +3,11 @@ export default {
   //   'biome check --write --no-errors-on-unmatched',
   // ],
   //
-  '*.{js,ts,cjs,mjs,d.cts,d.mts,jsx,tsx,json,jsonc,css,html}': [
+  '*.{js,ts,cjs,mjs,d.cts,d.mts,jsx,tsx,json,jsonc,css,html,vue}': [
     'prettier --write',
   ],
+  // eslint检查
+  '*.{js,ts,cjs,mjs,d.cts,d.mts,jsx,tsx,vue}': ['eslint --fix'],
   '**/*.ts?(x)': () => 'tsgo -p tsconfig.json --noEmit',
   '**/*.md': 'markdownlint --fix',
 }
